97ba7033acdb9f4295114d4904181818fd3e4d3a
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-07-02 07:32:50 +1000

    Insert CSS during config phase

--- app/templates/lib/name.js
+++ app/templates/lib/name.js
@@ -3,9 +3,10 @@ var classes = require('bespoke-classes');
 var insertCss = require('insert-css');
 
 module.exports = function() {
+  var css = fs.readFileSync(__dirname + '/tmp/theme.css', 'utf8');
+  insertCss(css, { prepend: true });
+
   return function(deck) {
     classes()(deck);
-    var css = fs.readFileSync(__dirname + '/tmp/theme.css', 'utf8');
-    insertCss(css, { prepend: true });
   };
 };
